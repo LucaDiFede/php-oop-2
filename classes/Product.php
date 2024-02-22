1d86cf6aacba6c3453dbcadcd993f91a2615f1ec
LucaDiFede <146111756+LucaDiFede@users.noreply.github.com>
2024-02-22 21:54:27 +0100

    Initial Commit

---
+++ classes/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+class Product {
+
+    public $name;
+    public $image;
+    public $price;
+    public $quantity;
+    public $description;
+    public $category;
+
+
+public function __construct(
+    $name = null,
+    $image = null,
+    $price = null,
+    $quantity = null,
+    $description = null,
+    $category = null,
+)
+{
+    
+    $this->name = $name;
+    $this->image = $image;
+    $this->price = $price;
+    $this->quantity = $quantity;
+    $this->description = $description;
+    $this->category = $category;
+}
+}
